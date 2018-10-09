8a418406fb720f3cd95bbbbece7f02436fa81764
Rifai07 <43748908+Rifai07@users.noreply.github.com>
2018-10-10 00:26:50 +0800

    Create stack.java

---
+++ stack.java
@@ -0,0 +1,12 @@
+
+public class stack {
+	//Attributtes
+			String[] value = new String[100];
+			int top;
+			
+			//Method setter
+				String setvalueAt(int position, String value){
+					return value;
+				}
+
+}
